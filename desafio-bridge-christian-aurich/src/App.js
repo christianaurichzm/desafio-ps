9816a4097e0b56cc9c1b5810d9c655fdd538eca5
Christian Aurich Zanettini Martins <christian.aurichzm@gmail.com>
2020-03-19 07:09:01 -0300

    Add Header component

--- desafio-bridge-christian-aurich/src/App.js
+++ desafio-bridge-christian-aurich/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Header from "./frontend/components/Header";
 import Home from "./frontend/views/Home";
 
 const App = () => (
   <div className="App">
+    <Header />
     <Home />
   </div>
 )
---
+++ desafio-bridge-christian-aurich/src/frontend/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Header = () => (
+   <h1 className="header">
+       Desafio Bridge_ - Christian Aurich Zanettini Martins
+   </h1>
+);
+
+export default Header;
