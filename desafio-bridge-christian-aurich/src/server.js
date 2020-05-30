0a6ba52782db94a2b033cacc347e65d110cc88b1
Christian Aurich Zanettini Martins <christian.aurichzm@gmail.com>
2020-05-30 04:36:51 -0300

    Major improvement in the number checker algorithm

--- desafio-bridge-christian-aurich/src/server.js
+++ desafio-bridge-christian-aurich/src/server.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-undef */
-const express = require("express");
-const helmet = require("helmet");
-const bodyParser = require("body-parser");
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,24 +8,31 @@ app.use(helmet());
 
 const port = 5000;
 
-app.get("/", (req, res) => {
-    res.send({ express: "Express server" });
+app.get('/', (req, res) => {
+    res.send({ express: 'Express server' });
 });
 
-app.post("/number-checker", (req, res) => {
+app.post('/number-checker', (req, res) => {
     const number = parseInt(req.body.number);
+    const numberSquareRoot = parseInt(Math.sqrt(number))    
+    const divisors = [];
     let isPrime = false;
-    const divisors = [];    
 
-    for (let i = 1; i <= number; i++) {
+    for (let i = 1; i <= numberSquareRoot; i++) {
         if (number % i === 0) {
             divisors.push(i);
+
+            if (i !== number / i) {
+                divisors.push(number / i);
+            }
         }
     }
 
     if (divisors.length === 2) {
         isPrime = true;
     }
+
+    divisors.sort((a, b) => a - b);
 
     res.json({ divisors, isPrime });
 });
